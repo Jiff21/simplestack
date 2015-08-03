e20903fdcbba687e91195c6eb2f8c4e162ce8986
Jeffrey P Campbell <inajiff@gmail.com>
2015-08-02 20:58:46 -0700

    Starting using a layout to load navs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,32 @@
 if (Meteor.isClient) {
+	console.log('Starting Routing.');
+	// Router.route('/', function () {
+	// 	this.render('Home');
+	// });
+
+	Router.route('home', {
+		path: '/',
+			layoutTemplate: 'ApplicationLayout',
+		yieldTemplates: {
+			'topNav': {to: 'menu'},
+			'home': {to: 'article'},
+			'footer': {to: 'footer'}
+	  	}
+	});
+	Router.route('login', {
+		path: '/login',
+			layoutTemplate: 'ApplicationLayout',
+		yieldTemplates: {
+			'topNav': {to: 'menu'},
+			'login': {to: 'article'},
+			'footer': {to: 'footer'}
+	  	}
+	});	
+
+	Router.route('/ApplicationLayout');
+
+	console.log('Routing Finished Sucessfully.');
+
 	// // counter starts at 0
 	// Session.setDefault('counter', 0);
 
@@ -8,14 +36,6 @@ if (Meteor.isClient) {
 	// 	}
 	// });
 
-
-	Router.route('/', function () {
-  		this.render('Home');
-	});
-
-	Router.route('/login');
-
-	console.log('testing.')
 
 	// Template.hello.events({
 	// 	'click button': function () {
